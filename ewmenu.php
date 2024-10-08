c6eacf26138f5eac6102c0bfbd5249793ed131dc
Annisa Aulia <annisaauliarohim97@gmail.com>
2024-10-08 10:26:13 +0700

    update icon

--- ewmenu.php
+++ ewmenu.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace PHPMaker2020\ppei_20;
 
 // Menu Language
@@ -11,6 +12,11 @@ if ($Language && function_exists(PROJECT_NAMESPACE . "Config") && $Language->Lan
 	$MenuLanguage = new Language();
 }
 
+// Add the following lines for the icons
+echo '<!-- ICONS -->';
+echo '<script src="https://unpkg.com/@phosphor-icons/web"></script>';
+echo '<link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">';
+
 // Navbar menu
 $topMenu = new Menu("navbar", TRUE, TRUE);
 echo $topMenu->toScript();
@@ -19,29 +25,29 @@ echo $topMenu->toScript();
 $sideMenu = new Menu("menu", TRUE, FALSE);
 $sideMenu->addMenuItem(77, "mi_beranda", $MenuLanguage->MenuPhrase("77", "MenuText"), $MenuRelativePath . "beranda.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}beranda.php'), FALSE, FALSE, "fa-home", "", FALSE);
 //$sideMenu->addMenuItem(98, "mi_cv_pelrepes", $MenuLanguage->MenuPhrase("98", "MenuText"), $MenuRelativePath . "cv_pelrepeslist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}cv_pelrepes'), FALSE, FALSE, "fa-user", "", FALSE);
-$sideMenu->addMenuItem(19, "mi_t_peserta", $MenuLanguage->MenuPhrase("19", "MenuText"), $MenuRelativePath . "t_pesertalist.php?cmd=resetall", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_peserta'), FALSE, FALSE, "fa-user", "", FALSE);
-$sideMenu->addMenuItem(18, "mi_t_perusahaan", $MenuLanguage->MenuPhrase("18", "MenuText"), $MenuRelativePath . "t_perusahaanlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_perusahaan'), FALSE, FALSE, "fa-industry", "", FALSE);
+$sideMenu->addMenuItem(19, "mi_t_peserta", $MenuLanguage->MenuPhrase("19", "MenuText"), $MenuRelativePath . "t_pesertalist.php?cmd=resetall", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_peserta'), FALSE, FALSE, "bx bx-male-female", "", FALSE);
+$sideMenu->addMenuItem(18, "mi_t_perusahaan", $MenuLanguage->MenuPhrase("18", "MenuText"), $MenuRelativePath . "t_perusahaanlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_perusahaan'), FALSE, FALSE, "bx bxs-business", "", FALSE);
 $sideMenu->addMenuItem(280, "mi_t_cp", $MenuLanguage->MenuPhrase("280", "MenuText"), $MenuRelativePath . "t_cplist.php", 18, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_cp'), FALSE, FALSE, "fa fa-circle-o", "", FALSE);
-$sideMenu->addMenuItem(69, "mi_t_biointruktur", $MenuLanguage->MenuPhrase("69", "MenuText"), $MenuRelativePath . "t_biointrukturlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_biointruktur'), FALSE, FALSE, "fa-address-card", "", FALSE);
-$sideMenu->addMenuItem(532, "mi_t_jdiklat", $MenuLanguage->MenuPhrase("532", "MenuText"), $MenuRelativePath . "t_jdiklatlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_jdiklat'), FALSE, FALSE, "fa fa-newspaper-o", "", FALSE);
-$sideMenu->addMenuItem(279, "mci_Rencana_Program", $MenuLanguage->MenuPhrase("279", "MenuText"), $MenuRelativePath . "javascript:void(0)", -1, "", IsLoggedIn(), FALSE, TRUE, "fa fa-newspaper-o", "", FALSE);
+$sideMenu->addMenuItem(69, "mi_t_biointruktur", $MenuLanguage->MenuPhrase("69", "MenuText"), $MenuRelativePath . "t_biointrukturlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_biointruktur'), FALSE, FALSE, "bx bxs-user", "", FALSE);
+$sideMenu->addMenuItem(532, "mi_t_jdiklat", $MenuLanguage->MenuPhrase("532", "MenuText"), $MenuRelativePath . "t_jdiklatlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_jdiklat'), FALSE, FALSE, "bx bxs-edit-alt", "", FALSE);
+$sideMenu->addMenuItem(279, "mci_Rencana_Program", $MenuLanguage->MenuPhrase("279", "MenuText"), $MenuRelativePath . "javascript:void(0)", -1, "", IsLoggedIn(), FALSE, TRUE, "bx bxs-notepad", "", FALSE);
 $sideMenu->addMenuItem(181, "mi_t_rpdiklat", $MenuLanguage->MenuPhrase("181", "MenuText"), $MenuRelativePath . "t_rpdiklatlist.php", 279, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_rpdiklat'), FALSE, FALSE, "fa fa-circle-o", "", FALSE);
 $sideMenu->addMenuItem(183, "mi_t_rpkerjasama", $MenuLanguage->MenuPhrase("183", "MenuText"), $MenuRelativePath . "t_rpkerjasamalist.php", 279, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_rpkerjasama'), FALSE, FALSE, "fa fa-circle-o", "", FALSE);
 $sideMenu->addMenuItem(187, "mi_t_rkcoaching", $MenuLanguage->MenuPhrase("187", "MenuText"), $MenuRelativePath . "t_rkcoachinglist.php", 279, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_rkcoaching'), FALSE, FALSE, "fa fa-circle-o", "", FALSE);
 $sideMenu->addMenuItem(528, "mi_t_rkwebinar", $MenuLanguage->MenuPhrase("528", "MenuText"), $MenuRelativePath . "t_rkwebinarlist.php", 279, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_rkwebinar'), FALSE, FALSE, "fa fa-circle-o", "", FALSE);
-$sideMenu->addMenuItem(533, "mi_panitia_pelat", $MenuLanguage->MenuPhrase("533", "MenuText"), $MenuRelativePath . "panitia_pelatlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}panitia_pelat'), FALSE, FALSE, "fa-user", "", FALSE);
-$sideMenu->addMenuItem(16, "mi_t_pelatihan", $MenuLanguage->MenuPhrase("16", "MenuText"), $MenuRelativePath . "t_pelatihanlist.php?cmd=resetall", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_pelatihan'), FALSE, FALSE, "fa-table", "", FALSE);
-$sideMenu->addMenuItem(636, "mi_webinar", $MenuLanguage->MenuPhrase("636", "MenuText"), $MenuRelativePath . "webinarlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}webinar'), FALSE, FALSE, "fa fa-calendar-o", "", FALSE);
-$sideMenu->addMenuItem(647, "mi_excp", $MenuLanguage->MenuPhrase("647", "MenuText"), $MenuRelativePath . "excplist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}excp'), FALSE, FALSE, "fa-list", "", FALSE);
-$sideMenu->addMenuItem(10, "mi_t_judul", $MenuLanguage->MenuPhrase("10", "MenuText"), $MenuRelativePath . "t_judullist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_judul'), FALSE, FALSE, "fa-list", "", FALSE);
-$sideMenu->addMenuItem(73, "mi_t_juduldetail", $MenuLanguage->MenuPhrase("73", "MenuText"), $MenuRelativePath . "t_juduldetaillist.php?cmd=resetall", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_juduldetail'), FALSE, FALSE, "fa-list-alt", "", FALSE);
-$sideMenu->addMenuItem(88, "mi_cv_coachingprogram", $MenuLanguage->MenuPhrase("88", "MenuText"), $MenuRelativePath . "cv_coachingprogramlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}cv_coachingprogram'), FALSE, FALSE, "fa-list", "", FALSE);
+$sideMenu->addMenuItem(533, "mi_panitia_pelat", $MenuLanguage->MenuPhrase("533", "MenuText"), $MenuRelativePath . "panitia_pelatlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}panitia_pelat'), FALSE, FALSE, "bx bxs-user", "", FALSE);
+$sideMenu->addMenuItem(16, "mi_t_pelatihan", $MenuLanguage->MenuPhrase("16", "MenuText"), $MenuRelativePath . "t_pelatihanlist.php?cmd=resetall", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_pelatihan'), FALSE, FALSE, "bx bx-list-ul", "", FALSE);
+$sideMenu->addMenuItem(636, "mi_webinar", $MenuLanguage->MenuPhrase("636", "MenuText"), $MenuRelativePath . "webinarlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}webinar'), FALSE, FALSE, "bx bxs-slideshow", "", FALSE);
+$sideMenu->addMenuItem(647, "mi_excp", $MenuLanguage->MenuPhrase("647", "MenuText"), $MenuRelativePath . "excplist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}excp'), FALSE, FALSE, "bx bxs-plane-alt", "", FALSE);
+$sideMenu->addMenuItem(10, "mi_t_judul", $MenuLanguage->MenuPhrase("10", "MenuText"), $MenuRelativePath . "t_judullist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_judul'), FALSE, FALSE, "bx bxs-book-content", "", FALSE);
+$sideMenu->addMenuItem(73, "mi_t_juduldetail", $MenuLanguage->MenuPhrase("73", "MenuText"), $MenuRelativePath . "t_juduldetaillist.php?cmd=resetall", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_juduldetail'), FALSE, FALSE, "bx bxs-book-bookmark", "", FALSE);
+$sideMenu->addMenuItem(88, "mi_cv_coachingprogram", $MenuLanguage->MenuPhrase("88", "MenuText"), $MenuRelativePath . "cv_coachingprogramlist.php", -1, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}cv_coachingprogram'), FALSE, FALSE, "bx bxs-note", "", FALSE);
 $sideMenu->addMenuItem(450, "mci_Report", $MenuLanguage->MenuPhrase("450", "MenuText"), "", -1, "", IsLoggedIn(), TRUE, TRUE, "", "", FALSE);
-$sideMenu->addMenuItem(639, "mi_evaluasi", $MenuLanguage->MenuPhrase("639", "MenuText"), $MenuRelativePath . "evaluasilist.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}evaluasi'), FALSE, FALSE, "fa-list", "", FALSE);
-$sideMenu->addMenuItem(526, "mi_realpengajar", $MenuLanguage->MenuPhrase("526", "MenuText"), $MenuRelativePath . "realpengajar.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}realpengajar.php'), FALSE, FALSE, "fa-address-card", "", FALSE);
-$sideMenu->addMenuItem(527, "mi_realpelatihan", $MenuLanguage->MenuPhrase("527", "MenuText"), $MenuRelativePath . "realpelatihan.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}realpelatihan.php'), FALSE, FALSE, "fa-table", "", FALSE);
-$sideMenu->addMenuItem(529, "mi_realpeserta", $MenuLanguage->MenuPhrase("529", "MenuText"), $MenuRelativePath . "realpeserta.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}realpeserta.php'), FALSE, FALSE, "fa-user", "", FALSE);
-$sideMenu->addMenuItem(109, "mi_petri", $MenuLanguage->MenuPhrase("109", "MenuText"), $MenuRelativePath . "petrilist.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}petri'), FALSE, FALSE, "fa-list", "", FALSE);
+$sideMenu->addMenuItem(639, "mi_evaluasi", $MenuLanguage->MenuPhrase("639", "MenuText"), $MenuRelativePath . "evaluasilist.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}evaluasi'), FALSE, FALSE, "bx bxs-edit", "", FALSE);
+$sideMenu->addMenuItem(526, "mi_realpengajar", $MenuLanguage->MenuPhrase("526", "MenuText"), $MenuRelativePath . "realpengajar.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}realpengajar.php'), FALSE, FALSE, "bx bxs-chalkboard", "", FALSE);
+$sideMenu->addMenuItem(527, "mi_realpelatihan", $MenuLanguage->MenuPhrase("527", "MenuText"), $MenuRelativePath . "realpelatihan.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}realpelatihan.php'), FALSE, FALSE, "bx bx-book-open", "", FALSE);
+$sideMenu->addMenuItem(529, "mi_realpeserta", $MenuLanguage->MenuPhrase("529", "MenuText"), $MenuRelativePath . "realpeserta.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}realpeserta.php'), FALSE, FALSE, "bx bxs-user-rectangle", "", FALSE);
+$sideMenu->addMenuItem(109, "mi_petri", $MenuLanguage->MenuPhrase("109", "MenuText"), $MenuRelativePath . "petrilist.php", 450, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}petri'), FALSE, FALSE, "bx bxs-bookmarks", "", FALSE);
 $sideMenu->addMenuItem(387, "mci_Admin_Only", $MenuLanguage->MenuPhrase("387", "MenuText"), "", -1, "", IsLoggedIn(), TRUE, TRUE, "", "", FALSE);
 $sideMenu->addMenuItem(68, "mi_t_pegawai", $MenuLanguage->MenuPhrase("68", "MenuText"), $MenuRelativePath . "t_pegawailist.php", 387, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_pegawai'), FALSE, FALSE, "fa fa-circle-o", "", FALSE);
 $sideMenu->addMenuItem(29, "mi_t_users", $MenuLanguage->MenuPhrase("29", "MenuText"), $MenuRelativePath . "t_userslist.php", 387, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_users'), FALSE, FALSE, "fa fa-circle-o", "", FALSE);
@@ -70,4 +76,3 @@ $sideMenu->addMenuItem(80, "mi_t_tahapan", $MenuLanguage->MenuPhrase("80", "Menu
 $sideMenu->addMenuItem(61, "mi_t_userlevels", $MenuLanguage->MenuPhrase("61", "MenuText"), $MenuRelativePath . "t_userlevelslist.php", 30, "", AllowListMenu('{046BD04F-8A8B-497E-98E3-47339F0B2FB6}t_userlevels'), FALSE, FALSE, "fa fa-circle-o", "", FALSE);
 $sideMenu->addMenuItem(635, "mci_Keluar_Aplikasi", $MenuLanguage->MenuPhrase("635", "MenuText"), $MenuRelativePath . "logout.php", -1, "", IsLoggedIn(), FALSE, TRUE, "fa fa-sign-out", "", FALSE);
 echo $sideMenu->toScript();
-?>
